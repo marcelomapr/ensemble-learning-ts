afc161f0d8f2a242a0f2aef244fc441b53db9cd0
Marcelo Rosa <marcelomapr@gmail.com>
2018-07-09 07:53:46 -0300

    firs

---
+++ original.R
@@ -0,0 +1,8 @@
+# importing libraries
+library(feather) # data import
+library(data.table) # data handle
+library(rpart) # decision tree method
+library(party) # decision tree method
+library(forecast) # forecasting methods
+library(randomForest) # ensemble learning method
+library(ggplot2) # visualizations
